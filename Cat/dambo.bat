2452f79406c68ffeea1fe4ef440a9c1ed05b2769
Dambo389 <150391167+Dambo389@users.noreply.github.com>
2024-03-10 10:45:55 +0200

    Add files via upload

---
+++ Cat/dambo.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:m1
+Echo 1 - Start
+Echo 2 - Update
+Echo 3 - Exit
+Set /p choice="d: "
+if not defined choice goto m1
+if "%choice%"=="3" (Exit)
+if "%choice%"=="2" (git clone https://github.com/Dambo389/update.git)
+if "%choice%"=="1" (    git clone https://github.com/Dambo389/Game.git
+start Game\Game\Games.bat
+exit)
+goto m1
+pause
